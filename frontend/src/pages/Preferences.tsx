32b867aff4824f25af8820303de89a7a0c897702
Carmen Dyck <carmen.dyck5@gmail.com>
2025-02-11 15:19:53 -0800

    Rename preferences page

--- frontend/src/pages/Preferences.tsx
+++ frontend/src/pages/Preferences.tsx
@@ -1,25 +1,25 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import './Preferences.css';
 
-const Tab2: React.FC = () => {
+const Preferences: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Preferences</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Preferences</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="Preferences" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab2;
+export default Preferences;
