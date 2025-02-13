9d750c3181e594ce36aa7f4d04bf9ae427adaebc
Amrita Moturi <amoturi@berkeley.edu>
2025-02-12 19:26:23 -0800

    added .css pages for CalendarView/Notifcations/Breaks

--- frontend/src/pages/CalendarView.tsx
+++ frontend/src/pages/CalendarView.tsx
@@ -1,21 +1,21 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import "@ionic/react/css/flex-utils.css"; // Ensure Ionic utilities are available
-import FullCalendar from "@fullcalendar/react"; // FullCalendar library
-import timeGridPlugin from "@fullcalendar/timegrid"; // Weekly & daily views
+import "@ionic/react/css/flex-utils.css"; 
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
 
 const CalendarView: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Weekly Calendar</IonTitle>
+          <IonTitle>My Calendar</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <div style={{ padding: "10px" }}>
           <FullCalendar
             plugins={[timeGridPlugin]}
-            initialView="timeGridWeek" // 👈 This sets the weekly view
+            initialView="timeGridWeek"
             height="auto"
             slotMinTime="08:00:00" // Set the earliest time visible
             slotMaxTime="20:00:00" // Set the latest time visible
