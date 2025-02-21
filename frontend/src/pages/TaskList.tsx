1e76dda2951f945f72f2a269b43ff455c14ea3fa
pangjiayang <j4pang@ucsd.edu>
2025-02-20 19:31:41 -0800

    Add tasklist screen

--- frontend/src/pages/TaskList.tsx
+++ frontend/src/pages/TaskList.tsx
@@ -1,15 +1,31 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { useState } from "react";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonCheckbox} from '@ionic/react';
 import { useHistory } from "react-router-dom";
 import './TaskList.css';
 
+const initialTasks = [
+  { id: 1, title: "Finish CSE210 homework", duration: "5h", dueDate: "Feb 15", category: "study", color: "red" },
+  { id: 2, title: "Prepare a presentation", duration: "2h", dueDate: "Feb 16", category: "study", color: "yellow" },
+  { id: 3, title: "Go to the Gym", duration: "1h", dueDate: "Feb 17", category: "personal", color: "green" },
+  { id: 4, title: "Plan your meal", duration: "20mins", dueDate: "Feb 18", category: "personal", color: "green" },
+  { id: 5, title: "Review daily goals before sleeping.", duration: "5mins", dueDate: "Feb 19", category: "personal", color: "green" }
+];
+
 const TaskList: React.FC = () => {
+  const [tasks, setTasks] = useState(initialTasks);
   const history = useHistory();
 
-  const selectTask = () => {
+  const selectTask = (taskId: number) => {
     console.log("moved to viewtask");
-    history.replace("/viewtask");
+    // history.push(`/viewtask/${taskId}`);
+    history.push(`/viewtask`);
     window.location.reload(); // TODO: Figure out how to remove this
+    //
+  };
+
+  const removeTask = (taskId: number) => {
+    const updatedTasks = tasks.filter(task => task.id !== taskId); // Filter out the selected task
+    setTasks(updatedTasks); // Update the state to remove the task
   };
 
   return (
@@ -20,15 +36,25 @@ const TaskList: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">TaskList</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-        <IonContent className="ion-flex ion-justify-content-center ion-align-items-center ion-padding">
-          <IonButton expand="full" onClick={selectTask}>Task A</IonButton>
-        </IonContent>
+
+        <IonList>
+          {tasks.map((task) => (
+          <IonItem key={task.id} className={`task-item ${task.color}`} onClick={() => selectTask(task.id)}>
+          <IonCheckbox
+            slot="start"
+            onClick={(e) => e.stopPropagation()} 
+            onIonChange={() => removeTask(task.id)}
+          />
+          <IonLabel> 
+            <h2>{task.title}</h2>
+            <p className="due-date">Due: {task.dueDate}</p>
+          </IonLabel>
+          <span className="duration">{task.duration}</span>
+        </IonItem>
+          ))}
+        </IonList>
+
+   
       </IonContent>
     </IonPage>
   );
