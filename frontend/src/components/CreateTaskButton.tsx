3ab5f5f4361d35cb813d39af332e9fb7f8b75b43
donle3 <d6le@ucsd.edu>
2025-02-20 19:22:33 -0800

    small refactor for CreateTaskButton.tsx (recommended by Iris in PR)

--- frontend/src/components/CreateTaskButton.tsx
+++ frontend/src/components/CreateTaskButton.tsx
@@ -3,7 +3,7 @@ import { add } from "ionicons/icons";
 import { useLocation, useHistory } from "react-router-dom";
 
 const excludedPages: string[] = ["/login", "/create_acct_pref",
-  "/create_acct_pref_pg2", "/createtask"]
+                                "/create_acct_pref_pg2", "/createtask"]
 
 const CreateTaskButton: React.FC = () => {
   const location = useLocation();
